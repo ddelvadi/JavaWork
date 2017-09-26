451338f8574ded92e473744d10b3b730a793063e
ddelvadi <ddelvadi@hawk.iit.edu>
2017-09-26 17:28:19 -0500

    Added code that displays each line on seperate lines

---
+++ PersonalInfo/src/PersonalInfo.java
@@ -0,0 +1,16 @@
+/**
+ * Problem - Write, compile, and test a class that displays your full name, e-mail address, and phone number on three separate lines on the screen. 
+ * Save the class as PersonalInfo.java.
+ * @author Dipen Delvadiya
+ *
+ */
+public class PersonalInfo {
+	public static void main(String[] args) 
+	{
+		System.out.println("Dipen Delvadiya");
+		System.out.println("ddelvadi@hawk.iit.edu");
+		System.out.println("xxx W xxx St, City, State-Zip");
+		System.out.println("(xxx) xxx-xxxx"); //print() method produces output as well
+		//When println() is used, insertion point after the method goes to next line
+	}
+}
